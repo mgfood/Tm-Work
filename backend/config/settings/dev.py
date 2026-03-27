52e484832f6341c0bffc77ce8b2acca38568c1fe
mgfood <mgurbanmuradow2010@gmail.com>
2026-03-27 12:25:21 +0300

    переход на SQLite

--- backend/config/settings/dev.py
+++ backend/config/settings/dev.py
@@ -4,26 +4,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-DB_ENGINE = env('DB_ENGINE', default='sqlite3')
-
-if DB_ENGINE == 'postgres':
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': env('DB_NAME', default='tmwork_dev'),
-            'USER': env('DB_USER', default='postgres'),
-            'PASSWORD': env('DB_PASSWORD', default='postgres'),
-            'HOST': env('DB_HOST', default='localhost'),
-            'PORT': env('DB_PORT', default='5432'),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 CORS_ALLOW_ALL_ORIGINS = True
 CSRF_TRUSTED_ORIGINS = [
